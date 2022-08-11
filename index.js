194cada1de51b8bb67d53d1b9720e6cc5e83222b
Piero Maddaleni <pieromaddaleni03@gmail.com>
2022-08-11 16:28:47 +0000

    what even are standards

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//custom csv parser lets gooooooo
+const { parse } = require('csv-parse/sync');
 const fs = require('fs');
 
 //https://www.freecodecamp.org/news/three-ways-to-title-case-a-sentence-in-javascript-676a9175eb27/
@@ -10,17 +10,57 @@ function titleCase(str) {
   return str.join(' ');
 }
 
-const input = fs.readFileSync('input.json', { encoding: 'utf-8', flag: 'r' });
-const unprocessedCities = JSON.parse(input);
+const rawZips = fs.readFileSync('places_and_zips.txt', { encoding: 'utf-8', flag: 'r' }).split('\n');
+const keys = rawZips.shift().split('|');
+
+const unprocessedZips = rawZips.map((zip) => {
+  const data = zip.split('|');
+  let final = {};
+
+  data.forEach((item, i) => {
+    final[keys[i]] = item;
+  });
+
+  return final;
+})
+
+const unprocessedZipCoords = JSON.parse(fs.readFileSync('coordsData.json', { encoding: 'utf-8', flag: 'r' }));
+const unprocessedPops = parse(fs.readFileSync('places_and_pops.csv', { encoding: 'utf-8', flag: 'r' }), { columns: true, skip_empty_lines: true });
 
 let processed = {};
-
-unprocessedCities.forEach((city) => {
-
-  const cityName = titleCase(city.cityName.replaceAll('"', ''));
-  const cityPopulation = isNaN(city.cityPopulation) ? Number(city.cityPopulation.replaceAll(',', '')) : city.cityPopulation;
-
-  processed[cityName] = cityPopulation;
+unprocessedPops.forEach((place) => {
+  processed[place.GEO_ID.split('US')[1]] = {
+    name: titleCase(place.NAME.replace('cdp, ', 'CDP, ')),
+    geoID: place.GEO_ID.split('US')[1],
+    population: Number(place.P1_001N),
+    zipCodes: [],
+    coordinates: []
+  }
 });
 
-fs.writeFileSync('output.json', JSON.stringify(processed, null, 2));
+unprocessedZips.forEach((zip) => {
+  if (zip.GEOID_PLACE_20 !== '' && processed[zip.GEOID_PLACE_20]) {
+    processed[zip.GEOID_PLACE_20].zipCodes.push(Number(zip.GEOID_ZCTA5_20));
+  }
+})
+
+unprocessedZipCoords.forEach((zip) => {
+  if (processed[zip.GEOID]) {
+    processed[zip.GEOID].coordinates = [Number(zip.INTPTLAT), Number(zip.INTPTLON)]
+  }
+})
+
+let processedNameAsKey = {};
+Object.keys(processed).forEach((key) => {
+  const item = processed[key];
+  processedNameAsKey[item.name] = item;
+})
+
+console.log(processedNameAsKey['Sandy Springs City, Georgia'])
+console.log(processedNameAsKey['Chicago City, Illinois'])
+
+fs.writeFileSync('output.json', JSON.stringify(processedNameAsKey, null, 2));
+fs.writeFileSync('keys.json', JSON.stringify(Object.keys(processedNameAsKey), null, 2));
+
+fs.writeFileSync('outputMin.json', JSON.stringify(processedNameAsKey));
+fs.writeFileSync('keysMin.json', JSON.stringify(Object.keys(processedNameAsKey)));
