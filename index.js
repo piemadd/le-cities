bf790cca0e242a7110bbfbd44ace5f07b2664705
Piero Maddaleni <pieromaddaleni03@gmail.com>
2022-08-11 00:55:35 +0000

    title case!

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 //custom csv parser lets gooooooo
 const fs = require('fs');
 
+//https://www.freecodecamp.org/news/three-ways-to-title-case-a-sentence-in-javascript-676a9175eb27/
+function titleCase(str) {
+  str = str.toLowerCase().split(' ');
+  for (var i = 0; i < str.length; i++) {
+    str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
+  }
+  return str.join(' ');
+}
+
 const input = fs.readFileSync('input.csv', { encoding: 'utf-8', flag: 'r' });
 const lines = input.split('\r\n');
 
 const processed = lines.map((line) => {
-  return line.replaceAll('"', '').replace(' city,', ',').replace(' town,', ',').replace(' village,', ',').replace(' borough,', ',');
+  return titleCase(line.replaceAll('"', ''));
 });
 
 fs.writeFileSync('output.json', JSON.stringify(processed, null, 2));
