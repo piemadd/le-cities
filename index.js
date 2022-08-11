87ad7b047d07cc799655fca9b2aa366d786a5a03
Piero Maddaleni <pieromaddaleni03@gmail.com>
2022-08-11 00:51:52 +0000

    village and borough replace

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const input = fs.readFileSync('input.csv', { encoding: 'utf-8', flag: 'r' });
 const lines = input.split('\r\n');
 
 const processed = lines.map((line) => {
-  return line.replaceAll('"', '').replace(' city,', ',').replace(' town,', ',');
+  return line.replaceAll('"', '').replace(' city,', ',').replace(' town,', ',').replace(' village,', ',').replace(' borough,', ',');
 });
 
 fs.writeFileSync('output.json', JSON.stringify(processed, null, 2));
