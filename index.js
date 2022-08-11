b5b4b430d6b887f8f1e6ce6e08f4273c13258278
Piero Maddaleni <pieromaddaleni03@gmail.com>
2022-08-11 00:49:23 +0000

    fixed names

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const input = fs.readFileSync('input.csv', { encoding: 'utf-8', flag: 'r' });
 const lines = input.split('\r\n');
 
 const processed = lines.map((line) => {
-  return line.replaceAll('"', '');
+  return line.replaceAll('"', '').replace(' city,', ',').replace(' town,', ',');
 });
 
 fs.writeFileSync('output.json', JSON.stringify(processed, null, 2));
